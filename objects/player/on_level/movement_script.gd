62a7885d600c9801d5c300afc2d831afe7b3c6d7
Iviv122 <ivan.skrinnik.nomia@gmail.com>
2025-10-21 19:43:41 +0200

    better ground check

--- objects/player/on_level/movement_script.gd
+++ objects/player/on_level/movement_script.gd
@@ -5,6 +5,7 @@ class_name CharacterMovement
 @export var SPEED = 300.0
 @export var JUMP_VELOCITY = -400.0
 
+var is_grounded : bool = false
 
 func check_is_drop_down():
 	pass
@@ -12,15 +13,36 @@ func check_is_drop_down():
 func drop():
 	position.y +=1
 
+func ground_check():
+	var space_state = get_world_2d().direct_space_state
+
+	var rectangle_shape = RectangleShape2D.new()
+
+	rectangle_shape.size.x = 32
+	rectangle_shape.size.y = 4 
+
+	var query = PhysicsShapeQueryParameters2D.new()
+	query.shape = rectangle_shape
+	query.transform = transform
+	query.transform.origin =global_position+Vector2.DOWN*18 
+	query.collide_with_areas = true 
+
+	var result = space_state.intersect_shape(query) 
+
+
+	is_grounded = result.size() > 0
+
+
 func _physics_process(delta: float) -> void:
 	
+	ground_check()
 	if Input.is_action_pressed("down"):
 		drop()
 
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	var direction := Input.get_axis("move_left", "move_right")
